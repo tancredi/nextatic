93bc7cdf8b08808b88fc9ff5679368cca9c424d5
Tancredi Trugenberger <tancredi.trugenberger@gmail.com>
2020-09-28 00:51:02 +0200

    Update packages

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,14 +6,18 @@ import { attributes, react as HomeContent } from '@content/home.md'
 export default class Home extends Component {
   render() {
     let { title, cats } = attributes
+
     return (
       <>
         <Head>
           <script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
         </Head>
+
         <article>
           <h1>{title}</h1>
+
           <HomeContent />
+
           <ul>
             {cats.map((cat, k) => (
               <li key={k}>
